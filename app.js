c5aede22b32a4f0137e5280c6f82eacc1af9fe8c
Bartek Ringwelski <bartekr@gmail.com>
2016-10-08 19:08:54 -0700

    fixed issue

--- app.js
+++ app.js
@@ -2,41 +2,29 @@ $(document).ready(function() {
     $("h1").text("to-do's");
 
 
-//    $('li').append("hey")
+    //    $('li').append("hey")
 
     $('input').on('keypress', function(event) {
-      if (event.which === 13) {
+        if (event.which === 13) {
 
-        var $value = $('input').val();
-        $('ul').append("<li>"+$value+"</li>");
-        $('input').val('');
-        $('li').last().append("<button>kill</button>")
+            var $value = $('input').val();
+            $('ul').append("<li>" + $value + "</li>");
+            $('input').val('');
+            $('li').last().append("<button>kill</button>")
 
-      }
-
-
-
-  $('button').on('click', function() {
-
-    var counter = 0;
-
-      $('button').on('click', function() {
-        console.log(counter);
-
-        $(this).parent().addClass('strikeThroughAndGray');
-        counter++;
-
-        if (counter > 0) {
-          $(this).parent().remove();
         }
 
 
-        // else{
-        //   console.log("adding class");
-        //  $('button').parent().addClass('strikeThroughAndGray')
-      //  }
 
+        var counter = 0;
+        $('button').on('click', function() {
+
+            $(this).parent().addClass('strikeThroughAndGray');
+
+            if (counter > 0) {
+                $(this).parent().remove();
+            }
+
+        });
     });
-  });
-});
 });
