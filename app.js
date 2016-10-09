38de155d49fadd9439866a627428110ac696b5e3
Bartek Ringwelski <bartekr@gmail.com>
2016-10-08 19:55:31 -0700

    brilliant nick used memoize strategy to make awesomeness

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 $(document).ready(function() {
     $("h1").text("to-do's");
 
-
-    //    $('li').append("hey")
-
     $('input').on('keypress', function(event) {
         if (event.which === 13) {
-
             var $value = $('input').val();
             $('ul').append("<li>" + $value + "</li>");
             $('input').val('');
-            $('li').last().append("<button>kill</button>")
+            $('li').last().append("<button> kill</button>")
+        };
 
-        }
+        var cache = {};
 
-
-
-        var counter = 0;
         $('button').on('click', function() {
-
-            $(this).parent().addClass('strikeThroughAndGray');
-
-            if (counter > 0) {
+            var arg = JSON.stringify(this);
+            if (cache[arg]) {
                 $(this).parent().remove();
+            } else {
+                $(this).parent().addClass('strikeThroughAndGray');
+                cache[arg] = true;
             }
-
         });
     });
 });
