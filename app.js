5f5b93479952ed90baaae6fd2ebc86e82bfe4a75
Bartek Ringwelski <bartekr@gmail.com>
2016-10-08 18:50:10 -0700

    messing around with jQuerty

---
+++ app.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $("h1").text("to-do's");
+
+
+//    $('li').append("hey")
+
+    $('input').on('keypress', function(event) {
+      if (event.which === 13) {
+
+        var $value = $('input').val();
+        $('ul').append("<li>"+$value+"</li>");
+        $('input').val('');
+        $('li').last().append("<button>kill</button>")
+
+      }
+
+
+
+      $('button').on('click', function() {
+        $(this).counter = 0; //not sure how to make set this outside scope
+
+        $(this).parent().addClass('strikeThroughAndGray')
+        $(this).counter++
+
+        if ($this.counter > 0) {
+          $(this).parent().remove();
+        }
+
+
+        // else{
+        //   console.log("adding class");
+        //  $('button').parent().addClass('strikeThroughAndGray')
+      //  }
+
+    });
+  });
+});
+
+
+
+$('.active').each(function(){
+   $(this).removeClass('active');
+});
+
+    if (jQuery('#image1').hasClass('crsl-active')){
+        jQuery(".pager1").toggleClass("active");
+    }
