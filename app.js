9e267ec112fbfc8e72cd87c28499ffe09f6431f5
Bartek Ringwelski <bartekr@gmail.com>
2016-10-09 17:00:01 -0700

    added animation to addition of bullets

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 $(document).ready(function() {
     $("h1").text("to-do's");
+
+    $("h1").after('<button id ="clear-all" >clear all</button');
+
+    $('#clear-all').on('click', function(){
+      $('ul').remove();
+      $('input').after('<ul id = "firstID"></ul>')
+    })
+
+
 var counter = 0;
 
     $('input').on('keypress', function(event) {
         if (event.which === 13) {
             var $value = $('input').val();
-            $('ul').append("<li class = 'li' data-counter =" + counter + ">" + $value + "</li>");
+        //    $('input').after('<ul id = "firstID"></ul>')
+           $('ul').append("<li class = 'li' data-counter =" + counter + ">" + $value + "</li>")
+
+
             counter++;
-            console.log(counter);
-            console.log("counter value is: " +$('.li').last().data('counter'))
             $('input').val('');
-            $('li').last().append("<button> kill</button>")
+            $('li').last('.li').append("#" +$('.li').last().data('counter'))
+            $('li').last('.li').append('<button class="del">del</button>')
+            $('li').last('.li').animate({opacity: '1.0'}, 150);
+
         };
 
         var cache = {};
 
-        $('button').on('click', function() {
+        $('.del').on('click', function() {
             var arg = JSON.stringify(this);
             if (cache[arg]) {
-                $(this).parent().remove();
+                $(this).parent().remove()
             } else {
                 $(this).parent().addClass('strikeThroughAndGray');
                 cache[arg] = true;
             }
         });
+
     });
 });
 //
